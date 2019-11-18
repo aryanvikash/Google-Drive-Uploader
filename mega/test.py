8ae5ce397c30fc21446b2d22c11ab3fb8431e43c
Aryan Vikash <31583400+aryanvikash@users.noreply.github.com>
2019-11-18 09:13:25 +0530

    Delete test.py

--- mega/test.py
+++
@@ -1,5 +0,0 @@
-import mega
-
-m = Mega.from_credentials("followaryan8@gmail.com","Vikash@12")
-                    filename=m.download_from_url(url)
-                    print("Downloading Complete Mega :", filename)
