6c2bab596ea08a87811a017548b9b24ae705d332
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-09-22 15:56:43 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -48,7 +48,7 @@ NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Us
 REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @Kamlesh_Kirodewal "
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
 ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n subscribe us  @HindiXstream "
-AUTH_URL = 'Generate And Copy Your Google Drive Token And Send It To Me \n 👉 <a href ="{}">Vist This Url</a> 👈'
+AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me' 
 UPLOADING = "Download Complete !! \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
