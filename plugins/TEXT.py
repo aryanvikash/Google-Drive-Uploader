7318727e83d1f267ad4207bd624c0e4fe8ce41ef
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-09-22 15:44:50 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -13,26 +13,29 @@ START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By usin
 
 HELP = """   <b>AUTHORISE BOT</b> 
        Use  /auth Command Generate
-       Your Google Drive Token And 
-       Send It To Bot  
-<b> You Wanna Change Your Login 
+       Your Google Drive
+       Token And 
+       Send It To Bot  \n
+       
+<b>    You Wanna Change Your Login 
         Account ?</b> \n
         You Can Use /revoke 
-        command            
-<b>What I Can Do With This Bot? </b>
-            You Can Upload Any Internet
-            Files On Your google
-            Drive Account.
-<b> Links Supported By Bot</b>
+        command       \n
+       
+<b>     What I Can Do With This Bot? </b>
+            •You Can Upload Any Internet
+            •Files On Your google
+             Drive Account.
+<b>     Links Supported By Bot</b>
             * Direct Links 
             * Openload links [Max Speed 
-              500 KBps :(   ]
+              5.6 Mbps :-)   ]
             * Dropbox links 
             *  Mega links
             
             + More On Its way:)
                 
-Bug Report @aryanvikash
+Bug Report @Kamlesh_Kirodewal
         """
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
@@ -41,17 +44,17 @@ DOWN_TWO = True
 DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :("
 DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @aryanvikash"
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @aryanvikash "
+NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @HindiXstream"
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @Kamlesh_Kirodewal "
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At @aryanvikash "
-AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
+ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n subscribe us  @HindiXstream "
+AUTH_URL = 'Generate And Copy Your Google Drive Token And Send It To Me \n 👉 <a href ="{}">Vist This Url</a> 👈'
 UPLOADING = "Download Complete !! \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At @aryanvikash"
+AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n subscribe us @HindiXstream"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
@@ -64,7 +67,7 @@ UPDATE = """ <b> Update  on  27.07.2019</b>
 <b> Links Supported By Bot</b>
             * Direct Links 
             * Openload links [Max Speed 
-              500 KBps :(   ]
+              5.6 MBps :-) ]
             * Dropbox links 
             *  Mega links (only files)
             
